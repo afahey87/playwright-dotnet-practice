39f3bc9a3272411062ed21ac26b095ccf62c346d
afahey87 <afahey87@gmail.com>
2023-10-05 10:55:08 -0400

    Started Add Product page

---
+++ AddProduct.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
+namespace AddProducts;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class Tests : PageTest
+{
+    [Test]
+    public async Task AddProductsTest()
+    {
+        await Page.GotoAsync("https://commitquality.com/");
+        var buttonAddProduct = Page.GetByTestId("add-a-product-button");
+        await buttonAddProduct.ClickAsync();
+
+        var titleText = Page.Locator(".container>div>h1");
+        await Expect(titleText).ToHaveTextAsync("Add Product");
+    }
+}
